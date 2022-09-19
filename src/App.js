deb92d29e2612a2151b2f76dec6cb7d6df6abaff
luseno-hub <luseno@outlook.com>
2022-09-19 17:11:05 +0300

    WIP: thinking in React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//START/////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+export function ProductRow() {
+  return <div>ProductRow</div>;
+}
+
+export function ProductCategoryRow() {
+  return <div>ProductCategoryRow</div>;
+}
+
+export function ProductTable() {
+  return <div>ProductTable</div>;
+}
+
+export function SearchBar({ handleChange, searchValue, checkedStatus }) {
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={handleChange}
+        value={searchValue}
+        placeholder="Search..."
+      ></input>
+      <div>
+        <input type="checkbox" onClick={checkedStatus}></input>
+        <span>Only show products in stock</span>
+      </div>
+    </div>
+  );
+}
+
+export function FilterableProductTable({ products }) {
+  const [searchValue, setSearchValue] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+  const checkedStatus = () => {
+    console.log("isChecked=>", "box checked!");
+    return !isChecked && setIsChecked(true);
+  };
+
+  function validate() {
+    if (document.querySelector(".sch").checked) {
+      alert("checked");
+    } else {
+      alert("You didn't check it! Let me check it for you.");
+    }
+  }
+  validate();
+
+  return (
+    <div>
+      <SearchBar
+        className="sch"
+        searchValue={searchValue}
+        handleChange={handleChange}
+        isChecked={checkedStatus}
+      ></SearchBar>
+      <ProductTable products={products}></ProductTable>
+    </div>
+  );
+}
+
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <FilterableProductTable products={PRODUCTS} />
+    </>
   );
 }
 
